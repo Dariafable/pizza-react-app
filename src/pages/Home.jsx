8fc9b8bdb88119c782c4a4b732384c1042c8e8c1
Daria Nebylitsa <daranebylitsa@gmail.com>
2022-08-26 19:26:20 +0300

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Sort from '../components/Sort';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://6304d4bc761a3bce77f0a0d5.mockapi.io/Items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='content__top'>
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
