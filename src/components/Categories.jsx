1c3947b9794918c0e0c76ac3574782319d975eaf
Daria Nebylitsa <daranebylitsa@gmail.com>
2022-09-17 15:06:21 +0300

    update

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
-const Categories = () => {
+const Categories = ({ value, onChangeCategory }) => {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
-  const onClickCategory = (i) => {
-    setActiveIndex(i);
-  };
 
   return (
     <div className='categories'>
       <ul>
-        {categories.map((value, i) => (
-          <li
-            key={i}
-            onClick={() => onClickCategory(i)}
-            className={activeIndex === i ? 'active' : ''}
-          >
-            {value}
+        {categories.map((categoryName, i) => (
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,59 @@
 import React from 'react';
 
+/* import { setCategoryId } from '../redux/slice/filterSlice'; */
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
 
 const Home = () => {
+  /*  const dispatch = useDispatch();
+  const categoryId = useSelector((state) => state.filter.categoryId);
+  console.log(categoryId); */
+
   const [items, setItems] = React.useState([]);
-  const [isLoading, setLoading] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    itemName: 'популярности',
+    sortProperty: 'rating',
+  });
+  const [sortOrder, setSortOrder] = React.useState(true);
+
+  /*  const onChangeCategory = (id) => {
+    dispatch(setCategoryId(id));
+  }; */
 
   React.useEffect(() => {
-    fetch('https://6304d4bc761a3bce77f0a0d5.mockapi.io/Items')
+    setIsLoading(true);
+    fetch(
+      `https://6304d4bc761a3bce77f0a0d5.mockapi.io/Items?${
+        categoryId > 0 ? `category=${categoryId}` : ''
+      }&sortBy=${sortType.sortProperty}&order=${sortOrder ? `asc` : `desc`}`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
-        setLoading(false);
+        setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType, sortOrder]);
 
   return (
     <div className='container'>
       <div className='content__top'>
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(i) =>
+            setCategoryId(i)
+          } /* value={categoryId} onChangeCategory={onChangeCategory} */
+        />
+        <Sort
+          value={sortType}
+          onChangeSort={(i) => setSortType(i)}
+          order={sortOrder}
+          onChangeSortOrder={(i) => setSortOrder(i)}
+        />
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>
