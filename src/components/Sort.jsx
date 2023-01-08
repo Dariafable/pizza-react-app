1b0bda045ceedff4db606ad179a7e5c44bfd7645
Daria Nebylitsa <daranebylitsa@gmail.com>
2023-01-08 17:55:37 +0300

    added an event listener to the sort-popup

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -12,6 +12,7 @@ const list = [
 const Sort = () => {
   const dispatch = useDispatch();
   const { sort, sortOrder } = useSelector((state) => state.filter);
+  const sortRef = React.useRef();
 
   const [open, setOpen] = React.useState(false);
 
@@ -24,8 +25,20 @@ const Sort = () => {
     dispatch(setSortOrder(i));
   };
 
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => document.body.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <div className='sort'>
+    <div ref={sortRef} className='sort'>
       <div className='sort__label'>
         <button
           className={sortOrder ? 'sort__asc' : 'sort__desc'}
